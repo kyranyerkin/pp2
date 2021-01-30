342ea5a79801b29e91b5e8cd31eff5e1bf9711b7
kyranyerkin <kyranyerkin527@gmail.com>
2021-01-30 21:39:35 +0600

    week1day2

--- week1/day2/6.py
+++ week1/day2/6.py
@@ -1,2 +1,14 @@
 a=int(input())
-print(str(int(a/60)%24)+" "+str(a-(int(a/60)*60)))
+s=""
+s+=str(int(a/3600)%24)+":"
+a=a-int(a/3600)*3600
+if int(a/60)>10:
+    s+=str(int(a/60))+":"
+else:
+    s += "0"+str(int(a / 60)) + ":"
+a=a-int(a/60)*60
+if int(a%60)>10:
+    s+=str(int(a%60))
+else:
+    s += "0"+str(int(a % 60))
+print(s)
